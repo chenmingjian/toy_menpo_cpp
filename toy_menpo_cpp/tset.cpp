1091b84d29230557317a568a70ab42e7a8523c34
chenmingjian <chen@chenmingjian.com>
2019-03-21 17:15:54 +0800

    test

--- toy_menpo_cpp/tset.cpp
+++ toy_menpo_cpp/tset.cpp
@@ -1,36 +1,16 @@
 ﻿#include <iostream>
 #include <opencv2/core.hpp>
 #include <typeinfo>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/opencv.hpp>
+
 
 using namespace cv;
 using namespace std;
 int main()
 {
-	vector<vector<Point2f> > shapes;
+	Ptr<xfeatures2d::SIFT> f2d = xfeatures2d::SIFT::create();
 
-	for (int i = 0; i < 150; ++i)
-	{
-		vector<Point2f> shape;
-		for (int j = 0; j < 19; ++j)
-		{
-			shape.push_back(Point2f(18 -j, j));
-		}
-		shapes.push_back(shape);
-	}
-	Mat all;
-	for (auto s : shapes)
-	{
-		Mat mat = Mat(s);
-		all.push_back(mat.t());
-	}
+	auto img = imread("C:\\Users\\chen\\Desktop\\data_set\\RawImage\\TrainingData\\001.bmp", 1);
 
-	cv::Mat sum_rows;
-	cv::reduce(all, sum_rows, 0, cv::REDUCE_SUM);
-	auto a = sum_rows / all.rows;
-	cv::Mat result(a);
-	cout << sum_rows << endl;
-	cout << result << endl;
-	cout << "row=" << result.rows << endl;
-	cout << "cols=" << result.cols << endl;
-    std::cout << "Hello World!\n"; 
 }
