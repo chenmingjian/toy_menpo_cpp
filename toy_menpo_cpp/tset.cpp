ad623ea87e8195c8c7477366f764561f7169af3e
chenmingjian <chen@chenmingjian.com>
2019-03-25 18:57:13 +0800

    renow

--- toy_menpo_cpp/tset.cpp
+++ toy_menpo_cpp/tset.cpp
@@ -1,16 +1,12 @@
 ﻿#include <iostream>
-#include <opencv2/core.hpp>
 #include <typeinfo>
-#include <opencv2/xfeatures2d.hpp>
 #include <opencv2/opencv.hpp>
 
 
-using namespace cv;
-using namespace std;
+
 int main()
 {
-	Ptr<xfeatures2d::SIFT> f2d = xfeatures2d::SIFT::create();
-
-	auto img = imread("C:\\Users\\chen\\Desktop\\data_set\\RawImage\\TrainingData\\001.bmp", 1);
-
+	cv::Mat img = cv::imread("C:\\Users\\chen\\Desktop\\data_set\\RawImage\\TrainingData\\001.bmp", 1);
+	img = img / 255;
+	return 0;
 }
